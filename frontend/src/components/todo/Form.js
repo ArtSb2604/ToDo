afc7b26108d64c095de636a53fc2744d1c767ff6
ArtSb2005 <sboev.art@gmail.com>
2023-04-13 02:08:29 +0300

    first commit

---
+++ frontend/src/components/todo/Form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Form extends Component {
+  render() {
+    return (
+      <div>
+        Форма
+      </div>
+    )
+  }
+}
+
+export default Form
